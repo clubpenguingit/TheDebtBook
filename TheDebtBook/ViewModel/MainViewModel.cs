4208eb54b1fe77ee168a987b3f12c4015873939a
Simon Jensen <clubpenguingit@gmail.com>
2019-03-11 10:12:17 +0100

    ViewModel Påbegyndt

---
+++ TheDebtBook/ViewModel/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿
+
+using Prism.Mvvm;
+using TheDebtBook.Model;
+
+namespace TheDebtBook.ViewModel
+{
+    public class MainViewModel : BindableBase
+    {
+        private Debtor _modelDebtor;
+
+        public Debtor ModelDebtor
+        {
+            get { return _modelDebtor; }
+            set
+            {
+                SetProperty(ref _modelDebtor, value);
+
+            }
+
+
+
+        }
+    }
+}
