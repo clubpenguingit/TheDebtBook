d49f842c05e8fa4fc20b0bdd2c8d0c08246471a8
tobiassandoelund <36983846+tobiassandoelund@users.noreply.github.com>
2019-03-11 09:53:55 +0100

    Model added - not done

--- TheDebtBook/Model/Debtor.cs
+++ TheDebtBook/Model/Debtor.cs
@@ -1,7 +1,27 @@
-﻿namespace TheDebtBook.Model
+﻿using System.Collections.Generic;
+
+namespace TheDebtBook.Model
 {
     public class Debtor
     {
-        
+        public Debtor(string name = "NN", double debt = 0)
+        {
+            Name = name;
+            Debt = debt;
+        }
+
+        public List<Payment> Type { get; set; }
+        public string Name  { get; set; }
+        public double Debt  { get; set; }
+
+        public void Borrow(double amount)
+        {
+
+        }
+
+        public void PayBack(double amount)
+        {
+
+        }
     }
 }
---
+++ TheDebtBook/Model/Payment.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace TheDebtBook.Model
+{
+    public class Payment
+    {
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+    }
+}
